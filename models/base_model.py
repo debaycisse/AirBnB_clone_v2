df29a76fee9f15e0fd615350a41669ffe2673466
Azeez Adebayo <debaycisse@gmail.com>
2024-04-20 19:16:49 +0100

    partially implement db storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -17,21 +17,30 @@ class BaseModel:
             args - is a list non-keyword arguments
             kwargs - is a list of keyword arguments
         """
-        self.id = Column(String(60), default=uuid.uuid4(), primary_key=True)
-        if not kwargs:
-            self.created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-            self.updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+        from os import getenv
+        if getenv('HBNB_TYPE_STORAGE') == 'db':
+            id = Column(String(60), default=uuid.uuid4(), primary_key=True)
+            if not kwargs:
+                created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+                updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+            else:
+                kwargs['updated_at'] = Column(DateTime, nullable=False, default=datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f'))
+                kwargs['created_at'] = Column(DateTime, nullable=False, default=datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f'))
+                if '__class__' in kwargs:
+                    del kwargs['__class__']
+                self.__dict__.update(kwargs)
         else:
-            kwargs['updated_at'] = Column(DateTime, nullable=False,
-                                          default=datetime.strptime(
-                                              kwargs['updated_at'],
-                                              '%Y-%m-%dT%H:%M:%S.%f'))
-            kwargs['created_at'] = Column(DateTime, nullable=False,
-                                          default=datetime.strptime(
-                                              kwargs['created_at'],
-                                              '%Y-%m-%dT%H:%M:%S.%f'))
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if not kwargs:
+                from models import storage
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                if '__class__' in kwargs:
+                    del kwargs['__class__']
+                self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -53,11 +62,11 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if _sa_instance_state in dictionary:
-            del dictionary._sa_instance_state
+        if '_sa_instance_state' in dictionary:
+            dictionary.pop('_sa_instance_state')
         return dictionary
 
     def delete(self):
         """Deletes the current instance or object of this class"""
         from models.__init__ import storage
-        del storage.delete(self)
+        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -3,12 +3,27 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class definition """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        'City', backref='state', cascade='all, delete, delete-orphan')
-    
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City', backref='state',
+            cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """Gets a list of city instances whose state_id is as equal
+            as this current instance or self"""
+            from models.city import City
+            from models.__init__ import storage
+
+            matched_cities = []
+            for v in storage.all(City).__dict__.values():
+                if v.state_id == self.id:
+                    matched_cities.append(v)
+            return matched_cities
