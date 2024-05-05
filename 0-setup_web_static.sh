f8694fdb3961e27d44e370d1ff90861c563ee8f5
Azeez Adebayo <debaycisse@gmail.com>
2024-05-04 23:55:51 -0700

    correct the code base to match the requirement

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
-# script sets up your web servers for the deployment of web_static
-
-# Install Nginx if not already installed
-if ! dpkg -s nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
+# A bash script that sets up your web servers for the deployment of web_static
+if ! command -v nginx &>/dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y nginx
 fi
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
 
-# Create a fake HTML file
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html &> /dev/null
+sudo mkdir -p /data/web_static/shared
 
-# Create symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Set ownership
-sudo chown -R ubuntu:ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data
 
 # Update Nginx configuration
 NGINX_CONFIG="/etc/nginx/sites-available/default"
-sudo sed -i "/^\s*location \/hbnb_static\s*{/a \ \ \ \ alias /data/web_static/current/;" "$NGINX_CONFIG"
+
+sed -i 's/server_name _;/server_name debaycisse.tech;/g' "$NGINX_CONFIG"
+
+sed -i '48,52d' "$NGINX_CONFIG"
+
+sed -i '48i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' "$NGINX_CONFIG"
 
 # Restart Nginx
 sudo service nginx restart
-
