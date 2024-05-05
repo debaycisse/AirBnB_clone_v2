6e7536e4e59c565b1b5ec9042bc83e7081d048b8
Azeez Adebayo <debaycisse@gmail.com>
2024-05-05 07:50:19 -0700

    correct the logic of this task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,16 +5,15 @@ web srver, using the Fabric modules"""
 from fabric.api import env, put, cd, sudo
 from os import path
 
+env.hosts = ['34.229.55.139', '54.90.32.207']
+env.user = 'ubuntu'
+
 
 def do_deploy(archive_path):
     """This function uses the Fabric module and its methods
     to distribute an archive to webservers"""
 
-    web_01 = '34.229.55.139'
-    web_02 = '54.90.32.207'
     try:
-        env.hosts = [web_01, web_02]
-        env.user = 'ubuntu'
         if not path.exists(archive_path):
             return False
         put(archive_path, "/tmp/")
@@ -22,11 +21,11 @@ def do_deploy(archive_path):
         with cd("/data/web_static/releases"):
             new_folder = filename.split('.')[0]
             sudo("mkdir -p {}".format(new_folder))
-            sudo("tar -xzf {} --directory={}/".format(
+            sudo("tar -xzf /tmp/{} --directory={}/".format(
                 filename, new_folder))
             sudo("rm -f /tmp/{}".format(filename))
             sudo("rm -f /data/web_static/current")
-            sudo("ln -s {} {}/".format(
+            sudo("ln -s {} {}".format(
                 new_folder, '/data/web_static/current'))
         return True
     except Exception as e:
