d42eec7ecd7ba8a00727d7c2440596f9c3757621
Azeez Adebayo <debaycisse@gmail.com>
2024-05-07 06:32:13 -0700

    implement task 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -5,12 +5,14 @@ exec { 'check_nginx':
 }
 
 # Create directories
-file { '/data/web_static/releases/test':
-  ensure => directory,
+exec { 'create a nested of folders for the first folders':
+  command => 'mkdir -p /data/web_static/releases/test',
+  path    => '/usr/bin',
 }
 
-file { '/data/web_static/shared':
-  ensure => directory,
+exec { 'create a nested of folders for the second folders':
+  command => 'mkdir -p /data/web_static/shared',
+  path    => '/usr/bin',
 }
 
 # Create index.html file
@@ -36,11 +38,14 @@ file { '/data':
 
 
 exec { 'insert_content_at_line_53':
-  command  => 'sed -i "53i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/releases/;\n\t}" /etc/nginx/sites-available/default',
+  command  => 'sed -i \'53i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/releases/;\n\t}\' /etc/nginx/sites-available/default',
   provider => shell,
 }
 
 # Restart Nginx
-exec { '/usr/bin/sudo /usr/sbin/service nginx restart':
+exec { 'restart nginx service':
+  command  => 'sudo service nginx restart',
+  path     => '/usr/bin',
   provider => 'shell',
 }
+
