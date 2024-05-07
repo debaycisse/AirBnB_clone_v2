442903b39f55aacc7d32086d4213996b2c9310e0
Azeez Adebayo <debaycisse@gmail.com>
2024-05-06 22:02:54 -0700

    implement task 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,36 +1,21 @@
-# A Puppet manifest to set up web servers for the deployment of web_static
-
-# Check if nginx is installed
-$nginx_installed = $::osfamily ? {
-  'Debian' => package { 'nginx':
-    ensure => installed,
-  },
-  default => undef,
-}
-
-# Install nginx if not already installed
-if $nginx_installed == undef {
-  package { 'nginx':
-    ensure => installed,
-    require => Package['update'],
-  }
+# check if nginx is installed
+exec { 'check_nginx':
+  command => '/usr/bin/apt-get update && /usr/bin/apt-get install -y nginx',
+  unless  => '/usr/bin/test -x /usr/sbin/nginx',
 }
 
 # Create directories
 file { '/data/web_static/releases/test':
   ensure => directory,
-  mode   => '0755',
 }
 
 file { '/data/web_static/shared':
   ensure => directory,
-  mode   => '0755',
 }
 
 # Create index.html file
 file { '/data/web_static/releases/test/index.html':
   ensure  => file,
-  mode    => '0644',
   content => "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n",
 }
 
@@ -41,42 +26,21 @@ file { '/data/web_static/current':
   force  => true,
 }
 
-# Change owner recursively
+# Change ownership recursively
 file { '/data':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
   recurse => true,
 }
 
-# Update Nginx configuration
-$nginx_config = '/etc/nginx/sites-available/default'
 
-file_line { 'update_server_name':
-  path    => $nginx_config,
-  line    => 'server_name debaycisse.tech;',
-  match   => 'server_name _;',
-  replace => true,
-}
-
-file_line { 'remove_hbnb_static_location':
-  path  => $nginx_config,
-  line  => 'location /hbnb_static/ {',
-  match => 'server_name debaycisse.tech;',
-}
-
-file_line { 'remove_hbnb_static_alias':
-  path  => $nginx_config,
-  line  => 'alias /data/web_static/current/;',
-  match => 'location /hbnb_static/ {',
+exec { 'insert_content_at_line_53':
+  command  => 'sed -i "53i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/releases/;\n\t}" /etc/nginx/sites-available/default',
+  provider => shell,
 }
 
 # Restart Nginx
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => [
-    File['/data'],
-    File_line['remove_hbnb_static_alias'],
-  ],
+exec { '/usr/bin/sudo /usr/sbin/service nginx restart':
+  provider => 'shell',
 }
