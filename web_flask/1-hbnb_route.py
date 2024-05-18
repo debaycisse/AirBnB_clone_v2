4085a4519ab9a565e23f53d786c5017c712c61fa
Azeez Adebayo <debaycisse@gmail.com>
2024-05-18 17:42:19 +0100

    ensure pep 8 compliance

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """This module houses a definition of two flask application's routes"""
-
 from flask import Flask
 
 app = Flask(__name__)
-
 app.url_map.strict_slashes = False
+
 
 @app.route('/')
 def hello_hbnb():
     """This function handles and processes a query to route /"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
