4916936ba94d93d14bf7f1c8fd9815c9238459bb
Azeez Adebayo <debaycisse@gmail.com>
2024-05-18 17:36:38 +0100

    implement task 1. HBNB
    script starts a web application where the routes are just two in number

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module houses a definition of two flask application's routes"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_hbnb():
+    """This function handles and processes a query to route /"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """This function handles and processes a query to route /hbnb"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
